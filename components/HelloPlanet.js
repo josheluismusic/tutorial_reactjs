d68771849e057a69ee37d6a8b4488de64094fe09
josheluis <josheluis.music@gmail.com>
2017-11-08 17:03:38 -0300

    cambios test

--- components/HelloPlanet.js
+++ components/HelloPlanet.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import {PlanetProperties} from './PlanetProperties';
 
 export class HelloPlanet extends React.Component {
+  constructor (props) {
+    super(props)
+    console.log(props)
+    this.state = {
+      counter: 0
+    }
+    this.updateThisCounter = this.updateThisCounter.bind(this)
+  }
+
+  updateThisCounter(){
+    this.setState({counter: this.state.counter + 1})
+  }
+
   render () {
-    console.log(this.props)
-    return <h1> Hey! You are on {this.props.name} your favorite number is {this.props.myObj.myFavNumber}</h1>
+    return <div>
+              <span>{this.state.counter}</span>
+              <PlanetProperties triggerParentUpdate={this.updateThisCounter}/>
+           </div>
   }
 }
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import {HelloPlanet} from './components/HelloPlanet'
-
+/*
 var obj = {
   myFavNumber: 12
 }
-
+*/
 ReactDom.render(
   <div>
-    <HelloPlanet name='Joshe' myObj={obj} />
+    <HelloPlanet name='Joshe' num={5} />
   </div>,
   document.getElementById('firstapp')
 )
